b9a6cf32b73b0e5d8e451bda2d298c745935f62f
nikita139584 <staskonikita139@gmail.com>
2026-07-27 21:58:56 +0200

    Fix image paths

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,10 @@
 import "/src/App.css";
 import { Link } from "react-router-dom";
 
-
 function Header() {
-
     return (
         <header className="header">
 
-            {/* ===== 1. НАВИГАЦИЯ ===== */}
-
-            {/* ===== 2. HERO ===== */}
             <section className="hero">
 
                 {/* ===== MAC ===== */}
@@ -24,28 +19,23 @@ function Header() {
                                 Купить
                             </button>
                         </Link>
-
                     </div>
 
-                        <img
-                            className="hero-image"
-                            src="/img/MackbockHeader.png"
-                            alt="MacBook Air"
-                        />
-
+                    <img
+                        className="hero-image"
+                        src="/img/MackbockHeader.png"
+                        alt="MacBook Air"
+                    />
                 </div>
-
 
                 {/* ===== iPHONE ===== */}
                 <div className="hero-content hero-iphone big">
 
-
-                        <img
-                            className="hero-image"
-                            src="../../public/img/hero_iphone_family__be5jkzxszb1e_large_2x.jpg"
-                            alt="iPhone"
-                        />
-
+                    <img
+                        className="hero-image"
+                        src="/img/hero_iphone_family__be5jkzxszb1e_large_2x.jpg"
+                        alt="iPhone"
+                    />
 
                     <h1 className="hero-title">iPhone</h1>
                     <h2>Познакомьтесь с новой линейкой iPhone.</h2>
@@ -58,26 +48,21 @@ function Header() {
                         </Link>
                     </div>
 
-
-
                 </div>
-                {/* ===== Ipad ===== */}
+
+                {/* ===== iPad ===== */}
                 <div className="hero-content hero-ipad big">
 
+                    <img
+                        className="hero-image"
+                        src="/img/iPadHeader.jpg"
+                        alt="iPad"
+                    />
 
-
-                        <img
-                            className="hero-image"
-                            src="/img/iPadHeader.jpg"
-                            alt="iPad"
-                        />
-
-
-                    <h1 className="hero-title">Ipad</h1>
+                    <h1 className="hero-title">iPad</h1>
                     <h2>Теперь ещё мощнее благодаря M4.</h2>
 
                     <div className="hero-buttons">
-
                         <Link to="/iPad">
                             <button className="hero-button-primary">
                                 Купить
@@ -85,26 +70,21 @@ function Header() {
                         </Link>
                     </div>
 
-
                 </div>
 
-
-                {/* ===== Mackbook ===== */}
+                {/* ===== MacBook Pro ===== */}
                 <div className="hero-content-mini dark">
 
+                    <img
+                        className="hero-image"
+                        src="/img/MackbookProHeader.jpg"
+                        alt="MacBook Pro"
+                    />
 
-                        <img
-                            className="hero-image"
-                            src="../../public/img/MackbookProHeader.jpg"
-                            alt="MackbookProHeader"
-                        />
-
-
-                    <h1 className="hero-title">MackBook Pro</h1>
+                    <h1 className="hero-title">MacBook Pro</h1>
                     <h2>Теперь с M5, M5 Pro и M5 Max.</h2>
 
                     <div className="hero-buttons">
-
                         <Link to="/mac">
                             <button className="hero-button-secondary">
                                 Купить
@@ -112,19 +92,16 @@ function Header() {
                         </Link>
                     </div>
 
-
                 </div>
 
-                {/* ===== Airpodspro===== */}
+                {/* ===== AirPods Pro ===== */}
                 <div className="hero-content-mini dark">
 
-
-                        <img
-                            className="hero-image"
-                            src="../../public/img/AirpodsproHeader.jpg"
-                            alt="AirpodsproHeader"
-                        />
-
+                    <img
+                        className="hero-image"
+                        src="/img/AirpodsproHeader.jpg"
+                        alt="AirPods Pro"
+                    />
 
                     <h1 className="hero-title">AirPods Pro 3</h1>
                     <h2>Лучшее в мире активное шумоподавление среди внутриканальных наушников.</h2>
@@ -135,29 +112,20 @@ function Header() {
                                 Купить
                             </button>
                         </Link>
-
-
-
                     </div>
-
 
                 </div>
 
-
-
-
-                {/* ===== Watchseries11===== */}
+                {/* ===== Watch ===== */}
                 <div className="hero-content-mini dark">
 
+                    <img
+                        className="hero-image"
+                        src="/img/Watchseries11Header.jpg"
+                        alt="Watch Series 11"
+                    />
 
-                        <img
-                            className="hero-image"
-                            src="../../public/img/Watchseries11Header.jpg"
-                            alt="Watchseries11"
-                        />
-
-
-                    <h1 className="hero-title">𝗪𝗔𝗧𝗖𝗛 SERIES 11</h1>
+                    <h1 className="hero-title">WATCH SERIES 11</h1>
                     <h2>Лучший способ заботиться о своём здоровье.</h2>
 
                     <div className="hero-buttons">
@@ -168,21 +136,16 @@ function Header() {
                         </Link>
                     </div>
 
-
                 </div>
 
-
-
-                {/* ===== ipadPro===== */}
+                {/* ===== iPad Pro ===== */}
                 <div className="hero-content-mini">
 
-
-                        <img
-                            className="hero-image"
-                            src="../../public/img/ipadProHeader.jpg"
-                            alt="ipadPro"
-                        />
-
+                    <img
+                        className="hero-image"
+                        src="/img/ipadProHeader.jpg"
+                        alt="iPad Pro"
+                    />
 
                     <h1 className="hero-title">iPad Pro</h1>
                     <h2>Передовой искусственный интеллект и революционные возможности.</h2>
@@ -195,22 +158,16 @@ function Header() {
                         </Link>
                     </div>
 
-
                 </div>
 
-
-                {/* ===== Apple Card===== */}
-
-
             </section>
+
             <div className="Endes">
-                <h1>Безграничные развлечения.
-                </h1>
-
-
+                <h1>Безграничные развлечения.</h1>
             </div>
+
         </header>
     );
-}   
+}
 
 export default Header;
